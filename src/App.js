6909d476321fb3039c2c52c55f6f83a9d7498688
Rathindu Wathsala <rathindu.wathsala@ayra.com.au>
2022-09-26 15:11:07 +0530

    initial test setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import RobotTile from './Images/robot-tile.png';
+import BackgroundTile from './Images/empty-tile.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <td><img src={RobotTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+        </tr>
+        <tr>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+        </tr>
+        <tr>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+        </tr>
+        <tr>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+        </tr>
+        <tr>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+          <td><img src={BackgroundTile} /></td>
+        </tr>
+      </table>
     </div>
   );
 }
