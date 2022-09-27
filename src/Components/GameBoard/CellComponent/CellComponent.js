ecfa632cf6628c2b1aeef11515e8f77a7141aa5f
Rathindu Wathsala <rathindu.wathsala@ayra.com.au>
2022-09-27 18:18:13 +0530

    removed memo from cellComponent

--- src/Components/GameBoard/CellComponent/CellComponent.js
+++ src/Components/GameBoard/CellComponent/CellComponent.js
@@ -1,16 +1,15 @@
 import EmptyCellImage from 'Images/empty-tile.png';
 import RobotCellImage from 'Images/robot-tile.png';
 import PropTypes from 'prop-types';
-import { memo } from 'react';
 import "./index.css";
 
-const CellComponent = memo(({ isRobot, onClickHandler }) => {
+const CellComponent = ({ isRobot, onClickHandler }) => {
   return (
     <td onClick={onClickHandler}>
       <img className='tile-image' src={isRobot ? RobotCellImage : EmptyCellImage} />
     </td>
   )
-});
+};
 
 CellComponent.defaultProps = {
   isRobot: false,
